b90e80b62cddbbc6baa1801966f1b5db5d06529b
eflaskaLingoport <65240735+eflaskaLingoport@users.noreply.github.com>
2020-05-12 14:37:45 -0600

    Add files via upload

---
+++ lingoTest.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    #include<iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <ctime>
+using namespace std;
+int main()
+{
+    //A couple very barebones experiments to test string concatenations on Globalyzer i18n Express
+    int day;
+    int month;
+    int year;
+    cout << "Day: " << endl;
+    cin >> day;
+    cout << "Month: " << endl;
+    cin >> month;
+    cout << "Year: " << endl;
+    cin >> year;
+    cout << month << "/" << day << "/" << year << endl; // convention for United States.
+    string fname = "Eric";
+    string lname = "Flaska";
+    cout << "Hello, my name is " << fname << " " << lname; // convention for United States.
+}
